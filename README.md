9208bab4af167a7a7e7daa6c4297f71f68792e3e
Mike Martin <mikemartin1090@gmail.com>
2019-10-21 10:43:22 -0400

    Establish my environment

---
+++ README.md
@@ -0,0 +1,5 @@
+#### Install with curl
+
+```shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/mikemartin1090/environmart/master/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,15 @@
+main() {
+  URL_PATH_TO_ENV_FILES="https://raw.githubusercontent.com/mikemartin1090/environmart/master"
+  FILE=".environmart"
+  curl -s "${URL_PATH_TO_ENV_FILES}/${FILE}" -o "${HOME}/${FILE}"
+  ENVS=".zshrc"
+  for env_file in $(echo $ENVS); do
+    if ! grep "${FILE}" "${HOME}/${env_file}" > /dev/null; then
+      echo Installing environmart into ${env_file}
+      echo "source ${FILE}" >> "${HOME}/${env_file}"
+    fi
+  done
+  echo All Done! Open a new window!
+}
+
+main
