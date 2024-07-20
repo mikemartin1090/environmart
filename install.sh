24046cc20aeb020e59a44e4fa766a0e5df62df45
Mike Martin <mikemartin1090@users.noreply.github.com>
2024-07-20 00:54:44 -0400

    zsh is standard shell on Mac OS now

--- install.sh
+++ install.sh
@@ -11,39 +11,37 @@ function _commandExists() {
 
 main() {
   ENVIRONMART_LOAD_FILE=".environmart"
-  for env_file in .zshrc; do
-    if ! grep "${ENVIRONMART_LOAD_FILE}" "${HOME}/${env_file}" 2>&1 > /dev/null; then
-      # GITHUB_USERNAME=$(sh -c 'ssh -o "IdentitiesOnly=yes" git@github.com 2>&1 | grep "successfully authenticated" | cut -f2 -d" " | cut -f1 -d"!"')
-      # [ -z "$GITHUB_USERNAME" ] && _punt "You need to setup your key with github"
+  if _osType linux \
+    && _commandExists apt-get; then
 
-      if _osType linux \
-      && _commandExists apt-get; then
-
-        for _command in curl zsh git; do
-          if ! _commandExists _command; then
-            APPS_TO_INSTALL="$APPS_TO_INSTALL $_command"
-          fi
-        done
-
-        if [ -n "$APPS_TO_INSTALL" ]; then
-          [ "$EUID" -ne 0 ] && SUDO="sudo "
-          eval "$SUDO apt-get update -y"
-          eval "$SUDO apt-get install -y $APPS_TO_INSTALL"
-        fi
-
+    for _command in curl zsh git; do
+      if ! _commandExists _command; then
+        APPS_TO_INSTALL="$APPS_TO_INSTALL $_command"
       fi
+    done
 
-      URL_PATH_TO_ENV_FILES="https://raw.githubusercontent.com/mikemartin1090/environmart/master"
-      if [ -f "./${ENVIRONMART_LOAD_FILE}" ]; then
-        cp "./${ENVIRONMART_LOAD_FILE}" "${HOME}/${ENVIRONMART_LOAD_FILE}"
-      else
-        curl -s "${URL_PATH_TO_ENV_FILES}/${ENVIRONMART_LOAD_FILE}" -o "${HOME}/${ENVIRONMART_LOAD_FILE}"
-      fi
-
-      echo Installing environmart into ${env_file}
-      echo "source ${HOME}/${ENVIRONMART_LOAD_FILE}" >> "${HOME}/${env_file}"
+    if [ -n "$APPS_TO_INSTALL" ]; then
+      [ "$EUID" -ne 0 ] && SUDO="sudo "
+      eval "$SUDO apt-get update -y"
+      eval "$SUDO apt-get install -y $APPS_TO_INSTALL"
     fi
-  done
+
+  fi
+
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  fi
+
+  ##TODO: update this to master (or whatever branch I go with) when I'm done
+  URL_PATH_TO_ENV_FILES="https://raw.githubusercontent.com/mikemartin1090/environmart/ubuntu-changes"
+  if [ -f "./${ENVIRONMART_LOAD_FILE}" ]; then
+    cp "./${ENVIRONMART_LOAD_FILE}" "${HOME}/${ENVIRONMART_LOAD_FILE}"
+  else
+    curl -s "${URL_PATH_TO_ENV_FILES}/${ENVIRONMART_LOAD_FILE}" -o "${HOME}/${ENVIRONMART_LOAD_FILE}"
+  fi
+
+  echo Installing environmart into ${env_file}
+  echo "source ${ENVIRONMART_LOAD_FILE}" >> "${HOME}/.zshrc"
   echo All Done! Open a new window!
 }
 
